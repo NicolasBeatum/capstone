6bd08d815e7f514ac08ece4db25fb28381ae8492
HansIgnaci0 <ha.mancilla@duocuc.cl>
2026-09-22 21:39:21 -0300

    Route: create /wellness/index.tsx re-exporting views/wellness and add diagnostic logging; remove top-level wellness.tsx

---
+++ DuocMind/src/app/wellness/index.tsx
@@ -0,0 +1,7 @@
+﻿import Wellness from '../views/wellness/wellness';
+
+// Debug import for Metro resolution
+// eslint-disable-next-line no-console
+console.log('DEBUG: wellness import', typeof Wellness, Wellness);
+
+export default Wellness;
