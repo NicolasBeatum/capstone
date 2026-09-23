c2eb914f08db3d82439d81baa99292bf49fe8004
HansIgnaci0 <ha.mancilla@duocuc.cl>
2026-09-22 21:46:06 -0300

    Fix login route index content: remove literal escape sequences and correct relative import to views/auth/login

--- DuocMind/src/app/login/index.tsx
+++ DuocMind/src/app/login/index.tsx
@@ -1,1 +1,8 @@
-﻿import Comp from './views/auth/login';\n\n// Diagnostic export: ensure router finds the view in views/ and helps debugging if undefined\n// eslint-disable-next-line no-console\nconsole.log('DEBUG: login import', typeof Comp, Comp);\n\nexport default Comp;\n
+import Comp from '../views/auth/login';
+
+// Diagnostic export: ensure router finds the view in views/ and helps debugging if undefined
+// eslint-disable-next-line no-console
+console.log('DEBUG: login import', typeof Comp, Comp);
+
+export default Comp;
+
