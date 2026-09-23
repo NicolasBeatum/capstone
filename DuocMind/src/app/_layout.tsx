8af9ec8ce14e01eb81766b5a63819efcab15f8f8
HansIgnaci0 <ha.mancilla@duocuc.cl>
2026-09-22 21:58:24 -0300

    Remove debug logs and restore direct Stack rendering in _layout; clean up diagnostics

--- DuocMind/src/app/_layout.tsx
+++ DuocMind/src/app/_layout.tsx
@@ -7,21 +7,9 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
 
-  // Debugging: log types to detect which import is undefined
-  try {
-    // eslint-disable-next-line no-console
-    console.log('DEBUG: Stack type', typeof Stack, Stack);
-    // eslint-disable-next-line no-console
-    console.log('DEBUG: ThemeProvider type', typeof ThemeProvider, ThemeProvider);
-  } catch (e) {
-    // ignore
-  }
-
-  const StackComponent: any = Stack || (() => null);
-
   return (
     <ThemeProvider>
-      <StackComponent screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }} />
       <StatusBar style={isDark ? 'light' : 'dark'} />
     </ThemeProvider>
   );
